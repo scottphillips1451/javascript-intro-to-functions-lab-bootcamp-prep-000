deb002e2fc5cf54e8970aa3b78e740be95591b25
Scott Phillips <scottphillips1451@sbcglobal.net>
2018-06-13 14:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function say HiToGrandma(string) {
+function sayHiToGrandma(string) {
   if (string === string.toLowerCase) return "I can't hear you!"
 else if (string === string.toUpperCase) return "YES INDEED!"
 else if (string === "I love you, Grandma.") return "I love you, too."
